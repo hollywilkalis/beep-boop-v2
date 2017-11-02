4e19967f68c95d70f53e99564922e03268b746e8
Holly Wilkalis <holly.wilkalis@gmail.com>
2017-11-01 21:47:58 -0700

    Add scripts to meet first condition, take in number and return range from zero to number input

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 //back end logic begins here
+function createNumArray(a, b) {
+   var arr = [],
+       i = a;
 
+    while((arr[arr.length] = i) < b) {i++}
+    return arr;
+}
 
 //front end logic begins here
 $(document).ready(function() {
-  $("form#survey").submit(function(event) {
+  $("form#user-form").submit(function(event) {
     event.preventDefault();
-
-    $('#formid')[0].reset();
+    debugger;
+    var inputtedNumber = $("#number-input").val();
+    var newArray = createNumArray(0, inputtedNumber);
+    alert(newArray);
+    $("#output").text(newArray).val();
+    $(".output-box").show().fadeIn("slow");
+    $('#user-form')[0].reset();
   }); //close form submit
 }); //close ready function
